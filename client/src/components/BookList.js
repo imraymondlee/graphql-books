a92bfb1cb9fba7c3184f8671b88c49a25971a30e
imraymondlee <imleeraymond@gmail.com>
2019-09-17 00:02:57 -0700

    Add submission

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,15 +1,6 @@
 import React, {Component} from 'react';
-import {gql} from 'apollo-boost';
 import {graphql} from 'react-apollo';
-
-const getBooksQuery = gql`
-  {
-    books {
-      name
-      id
-    }
-  }
-`
+import {getBooksQuery} from '../queries/queries';
 
 class BookList extends Component {
   displayBooks() {
@@ -26,7 +17,6 @@ class BookList extends Component {
   }
 
   render() {
-    console.log(this.props);
     return (
       <div>
         <ul id="book-list">
